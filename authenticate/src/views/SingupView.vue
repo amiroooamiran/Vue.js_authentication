3434699490580ba841ae6d83a7674abdc9083c84
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2024-04-16 22:54:36 +0330

    📎 create singup viuw

---
+++ authenticate/src/views/SingupView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <Singup />
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import Singup from '@/components/Singup.vue'
+  
+  export default {
+    name: 'SingupView',
+    components: {
+      Singup
+    }
+  }
+  </script>
+  
