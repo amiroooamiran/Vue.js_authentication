8946aa2aac2fa423eb53738898f85fd512030ff2
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2024-04-16 17:17:27 +0330

    🔗 Config Base Home view

--- authenticate/src/views/HomeView.vue
+++ authenticate/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Login />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
