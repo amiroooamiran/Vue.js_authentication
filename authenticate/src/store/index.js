dd8cdb50b82f3875d489e89cfd93b82f8c1b25fd
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2024-04-16 18:43:09 +0330

    📎 Create initializeStore

--- authenticate/src/store/index.js
+++ authenticate/src/store/index.js
@@ -1,14 +1,24 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
-  },
-  getters: {
+    token: "",
+    user_id: "",
+    isAuthenticated: false
   },
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem('token')) {
+        state.token = localStorage.getItem('token');
+        state.user_id = localStorage.getItem('user_id');
+        state.isAuthenticated = true;
+      } else {
+        state.isAuthenticated = false;
+      }
+    },
   },
   actions: {
   },
   modules: {
   }
-})
+});
