0eade5e5e8827ae60dbd62de7c4955c9d6f9b190
amiroooamiran <amirhosseinamiran2004@gmail.com>
2024-04-18 09:12:19 -0400

    📎 Create ProfileView

---
+++ authenticate/src/views/ProfileView.vue
@@ -0,0 +1,16 @@
+<template>
+    <Profile />
+</template>
+<script>
+
+
+import Profile from "@/components/Profile.vue"
+export default {
+    name: 'ProfileView',
+    components: {
+    Profile
+    }
+}
+
+</script>
+  
