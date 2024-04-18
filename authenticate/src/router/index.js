8fcd0436788d25027c9daa6cd92a7dc8fc431e8b
amiroooamiran <amirhosseinamiran2004@gmail.com>
2024-04-18 09:14:51 -0400

    🖇️ add Routing of ProfileView

--- authenticate/src/router/index.js
+++ authenticate/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SingupView from '@/views/SingupView.vue'
+import ProfileView from '@/views/ProfileView.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/singup',
     name: 'singup',
     component: SingupView
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileView
   },
 ]
 
