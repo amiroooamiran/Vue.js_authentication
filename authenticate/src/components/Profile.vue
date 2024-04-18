06c120faeaa10bbde64d19edaf0bb084901387ef
amiroooamiran <amirhosseinamiran2004@gmail.com>
2024-04-18 09:13:10 -0400

    📎 Create Base Profile Component

---
+++ authenticate/src/components/Profile.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container py-md-5 py-xl-8">
+      <div class="row gy-4 align-items-center">
+        <div class="col-12 col-md-6 col-xl-7">
+          <h1>User Profile</h1>
+
+        </div>
+      </div>
+    </div>
+  </template>
+  
+
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped lang="scss">
+  
+  </style>
+  
