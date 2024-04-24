cf0c494da019dd5cc8b107cb864ce1422e786030
amirooo amiran <amiran.amirhossein@gmail.com>
2024-04-24 14:46:26 +0330

    📎 Create Reset Password views

---
+++ authenticate/src/views/ResetPasswordView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <ResetPassword/>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import ResetPassword from '@/components/ResetPassword.vue'
+  
+  export default {
+    name: 'ResetPasswordView',
+    components: {
+      ResetPassword
+    }
+  }
+  </script>
+  
